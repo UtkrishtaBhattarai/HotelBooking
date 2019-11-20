1b7c797ea2b210d436ae60f912c885315ab4cae0
xpsl412z <bhattarai.utk1@gmail.com>
2019-11-20 07:38:31 -0600

    Almost completed some validation remaining and splash screen remaining

--- app/src/main/java/com/example/hotelbooking/MainActivity.java
+++ app/src/main/java/com/example/hotelbooking/MainActivity.java
@@ -2,48 +2,18 @@ package com.example.hotelbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Calendar;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.time.chrono.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Calendar;
-import java.util.Date;
-
-
-import java.time.*;
-
 import android.widget.Toast;
-
-import java.util.Date;
-
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,22 +21,53 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinlocation, spinroom;
     TextView tvindate, tvoutdate;
     Button btncalc;
-    int year1=0, year2=0;
-    int month1=0, month2=0;
-    int day1=0, day2=0;
+    TextView tvroom, tvadult, tvkids;
+    int year2, year3;
+    int month2, month3;
+    int day2, day3;
     Button simpledatepicker1, simpledatepicker2;
+    int no_of_room;
+    TextView tvdayofstay, totalsuite, tvtotal, tvVat, tvroomno,tverr;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                   // Intent i=new Intent(getBaseContext(),FirstScreen.class);
+                  //  startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
         spinlocation = findViewById(R.id.spinlocation);
         spinroom = findViewById(R.id.spinroom);
+        totalsuite = findViewById(R.id.totalsuite);
+        tvtotal = findViewById(R.id.tvtotal);
+        tvVat = findViewById(R.id.tvVat);
+        tvroomno = findViewById(R.id.tvroomno);
+        tverr = findViewById(R.id.tverr);
         tvindate = findViewById(R.id.tvindate);
+        tvdayofstay = findViewById(R.id.tvdayofstay);
         tvoutdate = findViewById(R.id.tvoutdate);
         simpledatepicker1 = findViewById(R.id.simpledatepicker1);
         simpledatepicker2 = findViewById(R.id.simpledatepicker2);
+        tvadult = findViewById(R.id.tvadult);
+        tvkids = findViewById(R.id.tvkids);
+        tvroom = findViewById(R.id.tvroom);
         btncalc = findViewById(R.id.btncalc);
         String location[] = {"Bhaktapur", "Chitwan", "Kathmandu"};
         String room[] = {"Deluxe", "AC", "Platinum"};
@@ -102,19 +103,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    private void loadCalendar1() {
+    private void loadCalendar2() {
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        final int day = c.get(Calendar.DAY_OF_MONTH);
 
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String date = month + "/" + dayOfMonth + "/" + year;
-                tvindate.setText(date);
+                String date = "Checked Out At::" + month + "/" + dayOfMonth + "/" + year;
+                month3 = month;
+                day3 = dayOfMonth;
+                year3 = year;
+                tvoutdate.setText(date);
             }
 
         }, year, month, day);
@@ -122,44 +125,110 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void loadCalendar2() {
-        final Calendar c1 = Calendar.getInstance();
-        year1= c1.get(Calendar.YEAR);
-        month1 = c1.get(Calendar.MONTH);
-        day1= c1.get(Calendar.DAY_OF_MONTH);
+    private void loadCalendar1() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        final int day = c.get(Calendar.DAY_OF_MONTH);
 
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year1, int month1, int dayOfMonth1) {
-                String date = month1 + "/" + dayOfMonth1 + "/" + year1;
-                tvoutdate.setText(date);
-
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = "Checked In At::" + month + "/" + dayOfMonth + "/" + year;
+                month2 = month;
+                day2 = dayOfMonth;
+                year2 = year;
+                tvindate.setText(date);
             }
-        }, year1, month1, day1);
 
+        }, year, month, day);
         datePickerDialog.show();
+
 
         btncalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
+                if (TextUtils.isEmpty(tvindate.getText())) {
+                    tverr.setText("Please enter Checked in Date ");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(tvkids.getText())) {
+                    tverr.setText("Please enter number of Kids ");
+                    return;
+                }
+                if (TextUtils.isEmpty(tvadult.getText())) {
+                    tverr.setText("Please enter number of adults ");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(tvoutdate.getText())) {
+                    tverr.setText("Please enter Checked out Date ");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(tvroom.getText())) {
+                    tverr.setText("Please enter Number of Rooms ");
+                    return;
+                }
+
                 Calendar cal1 = Calendar.getInstance();
                 Calendar cal2 = Calendar.getInstance();
-
-                cal1.set(year1, month1, day1);
-                cal2.set(year2, month2, day2);
-
+                cal1.set(year2, month2, day2);
+                cal2.set(year3, month3, day3);
                 long millis1 = cal1.getTimeInMillis();
                 long millis2 = cal2.getTimeInMillis();
+                long diff = millis2 - millis1;
+                long diffDays = (diff / (86400000));
+                no_of_room = Integer.parseInt(tvroom.getText().toString());
+                double price;
+                double Total_Price;
+                double Grand_Total;
 
-                long diff=millis2-millis1;
-                long diffDays = diff / (24 * 60 * 60 * 1000);
 
-                Toast.makeText(MainActivity.this, "is"+diff, Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "First is"+year1+""+month1+""+day1, Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Second IS"+year2+""+month2+""+day2, Toast.LENGTH_SHORT).show();
+                String suite = spinroom.getSelectedItem().toString();
+
+                if (suite == "Deluxe") {
+
+                    price = 2000;
+                    Total_Price = price * no_of_room * diffDays;
+                    Grand_Total = (0.13) * Total_Price + Total_Price;
+
+                    tvdayofstay.setText("Total Days:"+diffDays);
+                    tvroomno.setText(("Number of Room(s):"+no_of_room));
+                    totalsuite.setText("Room Price Per Night:"+"2000");
+                    tvtotal.setText("Total:"+Total_Price);
+                    tvVat.setText("Grand Total:"+Grand_Total);
+                    Toast.makeText(MainActivity.this, "Total price is" + Grand_Total, Toast.LENGTH_SHORT).show();
+
+
+                } else if (suite == "AC") {
+
+                    price = 3000;
+                    Total_Price = price * no_of_room * diffDays;
+                    Grand_Total = (0.13) * Total_Price + Total_Price;
+                    tvdayofstay.setText("Total Days:"+diffDays);
+                    tvroomno.setText(("Number of Room(s):"+no_of_room));
+                    totalsuite.setText("Room Price Per Night:"+"3000");
+                    tvtotal.setText("Total:"+Total_Price);
+                    tvVat.setText("Grand Total:"+Grand_Total);
+                    Toast.makeText(MainActivity.this, "Total price is" + Grand_Total, Toast.LENGTH_SHORT).show();
+
+                } else if (suite == "Platinum") {
+                    price = 4000;
+                    Total_Price = price * no_of_room * diffDays;
+                    Grand_Total = (0.13) * Total_Price + Total_Price;
+                    tvdayofstay.setText("Total Days:"+diffDays);
+                    tvroomno.setText(("Number of Room(s):"+no_of_room));
+                    totalsuite.setText("Room Price Per Night:"+"4000");
+                    tvtotal.setText("Total:"+Total_Price);
+                    tvVat.setText("Grand Total:"+Grand_Total);
+                    Toast.makeText(MainActivity.this, "Total price is" + Grand_Total, Toast.LENGTH_SHORT).show();
+
+                }
 
             }
         });
@@ -167,8 +236,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
 }
+
+
+
+
+
+
 
